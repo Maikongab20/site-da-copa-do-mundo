a42cc59940062b04bf27981a72ac743de25319ed
Maikon Gabriel <maikon111gabriel@gmail.com>
2022-10-06 22:21:01 -0300

    trabalho quase finalizado

---
+++ src/js/players.js
@@ -0,0 +1,219 @@
+
+export const players = [
+  {
+    "id": 1,
+    "name": "Ethan Horvath",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/e-horvath-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=440a237021ae43f3bb4da7673bf0719a&amp;hash=994C8F5F2B3806D5432B76BEAA5AB117",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/mnt-bio-images/mnt/horvath/horvathplayerprofiletier1playernamejpg55.ashx?mw=1440&rev=0cc953180b06423daf64860fa2f93dbc"
+    },
+    "position": "Goalkeeper",
+    "numbers": "18",
+    "dateOfBirth": "Jun 09 1995",
+    "hometown": "Highlands Ranch, Co",
+    "height": "6'3",
+    "club": "Luton Town (England)",
+    "appearance": 7,
+    "cleansheets": 2,
+    "wins": 3,
+    "description": "Parte da próxima geração de goleiros competindo para manter a forte linhagem dos EUA na posição, o nativo do Colorado representa os EUA em todas as faixas etárias desde o Sub-14. Enquanto ainda crescia em seu quadro de 6-4, ele começou a trilhar sua carreira ao ingressar na Academia do lado norueguês Molde aos 16 anos. Depois de se tornar titular do clube aos 20 anos, Horvath se juntou ao clube belga Brugge em 2017, onde ajudou o Blauw-Zwart a conquistar o título da liga na temporada 2017-18 e agora é titular regular. Tendo feito sua estreia no MNT em 2016, Horvath subiu constantemente no ranking de profundidade de goleiros e será um concorrente importante para a posição que leva à Copa do Mundo da FIFA 2022 no Catar. Horvath disse que, se ele não fosse filho único, não tem certeza de como sua carreira no futebol pode ter evoluído, e isso porque quando sua mudança para o Molde se materializou aos 16 anos, seus pais - Peter e Deana - fizeram a viagem para a Noruega com dele. O goleiro disse que largou tudo, deixando seus empregos para servir como um sistema de apoio enquanto se ajustava à vida na Escandinávia. Além disso, enquanto ele treinava e jogava, eles trabalhavam na limpeza de prédios e na coleta de lixo para complementar a renda familiar em apoio ao seu sonho. Em sua terceira aparição pela equipe principal em novembro de 2018 contra a Itália, Horvath fez cinco defesas espetaculares a caminho de ganhar o prêmio de Melhor em Campo da Budweiser na derrota por 1 a 0 para os Azzurri."
+  },
+  {
+    "id": 2,
+    "name": "Sean Johnson",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/s-johnson-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=7d681693fdf1460583508935c46875d1&amp;hash=6E938E70203464798442FC90BCBC415D",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/mnt-bio-images/mnt/johnson_s/johnsonsplayerprofiletier1playernamejpg55.ashx?mw=1440&rev=adc8419355934eb391578892e5bad990"
+    },
+    "position": "Goalkeeper",
+    "numbers": 25,
+    "dateOfBirth": "May 31 1989",
+    "hometown": "Lilburn, ga",
+    "height": "6'3",
+    "club": "New York City FC",
+    "appearance": 10,
+    "cleansheets": 5,
+    "wins": 4,
+    " description": "Com um impressionante quadro de 6-3, Sean Johnson provou seu valor como um goleiro altamente eficaz, com reflexos afiados e uma percepção aguçada do jogo. Com dupla nacionalidade jamaciana e norte-americana, Johnson optou por seguir sua carreira com os Estados Unidos, jogou pela primeira vez no Campeonato Sub-20 da CONCACAF de 2009 e está no grupo de jogadores ativos do USMNT desde 2011. Ele começou sua carreira no clube sendo draftado pelo Chicago Fire em 2010, passando a jogar em 176 partidas, ganhando seis jogos sem sofrer gols. Em 2017, ele se mudou para o NYCFC e é o goleiro titular desde então. Sean está entrando no ciclo de qualificação de 2022 como um jogador veterano na esperança de causar impacto na qualificação e além. Ele fez sua primeira partida com a equipe principal na Copa Ouro da CONCACAF de 2013 em uma partida da fase de grupos contra a Costa Rica, fazendo uma defesa crucial aos 80 minutos e registrando uma folha limpa contra a perene potência da CONCACAF. O USMNT passou a ganhar o torneio. Desde então, Johnson fez mais seis aparições e está se posicionando para mais com a equipe. Fora do campo, Johnson é um entusiasta automotivo conhecido por seu amor por carros esportivos rápidos e exóticos. Ele também é um ávido fã de basquete que há muito admira a carreira de Kevin Durant. Ele também gosta de passar o tempo ao ar livre e é um ávido pescador."
+  },
+  {
+    "id": 3,
+    "name": "Matt Turner",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/matt-turner-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=542a4596d6c744c38531f90fb01952c7&amp;hash=C75726C9B3FC945017BD6494AD138B83",
+      "perfil": " "
+    },
+    "position": "Goalkeeper",
+    "numbers": 1,
+    "dateOfBirth": "Jun 24 1994",
+    "hometown": "Park ridge, N.J",
+    "height": "6'3",
+    "club": "Arsenal (england)",
+    "appearance": 19,
+    "cleansheets": 13,
+    "wins": 14,
+    " description": ""
+  },
+  {
+    "id": 4,
+    "name": "Reggie Cannon",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/r-cannon-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=47028a52a7c042d6948be9fdc83062a7&amp;hash=A91593F32F6F24658E2008622B01D464",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/deandre-yedlin/deandreyedlinp1tier1v4desktop2x/cannon-2021-playerprofiletier1playernamejpg55.ashx?mw=1440&rev=55b364c9b07b4904bf04b11e85edb300"
+    },
+    "position": "DEFENDER",
+    "numbers": 20,
+    "dateOfBirth": "Jun 11 1998",
+    "hometown": "grapevine, Texas",
+    "height": "5'10",
+    "club": "FC Dallas",
+    "appearance": 28,
+    "cleansheets": 1,
+    "wins": 3,
+    " description": " "
+  },
+  {
+    "id": 5,
+    "name": "Sergiño Dest",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/deandre-yedlin/deandreyedlinp1tier1v4desktop2x/dest-playerprofiletier1playernamejpg55.ashx?mw=1440&rev=75aa2a6b9a664653810f5946fb38ac6f",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/deandre-yedlin/deandreyedlinp1tier1v4desktop2x/dest-playerprofiletier1playernamejpg55.ashx?mw=1440&rev=75aa2a6b9a664653810f5946fb38ac6f"
+    },
+    "position": "DEFENDER",
+    "numbers": 2,
+    "dateOfBirth": "Nov 03 2000",
+    "hometown": "Alme-Stad, Netherlands",
+    "height": "5'8",
+    "club": "AC Milian (Italy)",
+    "appearance": 18,
+    "cleansheets": 2,
+    "wins": 2,
+    " description": "Integrante do ciclo Sub-20 desde março de 2018, fez oito aparições pela equipe neste ciclo… Ingressou nos EUA no Campeonato Sub-20 da Concacaf 2018 após a fase de grupos como uma das três mudanças de elenco … chegando, começando nos jogos das Eliminatórias contra a Costa Rica e Honduras, bem como a final contra o México … Nomeado para o torneio Melhor XI … Marcou seu primeiro gol pelos EUA com os Sub-20 contra a Venezuela no US Soccer Sub-20 de 2018 Men's Invitational … Fez sua estreia no Sub-20 contra a França em março de 2018 … Antes de ingressar nos Sub-20, jogou com o U-19 MNT no campo de cúpula da Seleção Nacional Juvenil em janeiro de 2018 … Um membro da FIFA U-2017 dos EUA 17 equipe da Copa do Mundo na Índia,aparecendo em quatro jogos pela equipe … Disputou nove jogos pelos sub-17 durante o ciclo 2016-17 … Conquistou sua primeira convocação para o YNT com os sub-17 em outubro de 2016, para um conjunto de amistosos contra Costa Rica. Assinou seu primeiro contrato profissional com o Ajax em 10 de dezembro de 2018 … Nos livros do lendário clube holandês até 2021 … Trabalhou em minutos regulares este ano com Jong Ajax, registrando 1356 minutos em 17 jogos … Marcou seu primeiro gol profissional para Jong Ajax em 30 de novembro de 2018 contra o Twente. O nome completo é Sergiño Gianni Dest… Também atende por Ser, Serg e Serra… O pai é surinamês-americano, a mãe é holandesa… Em seu tempo livre, gosta de jogar basquete, assistir filmes e ouvir música… Adora descontrair com os amigos, conversar e curtir a vida... Elenca a Copa do Mundo Sub-17 e o primeiro gol profissional como seus maiores momentos do futebol até agora... Jogador de futebol favorito e maior inspiração atlética é Ronaldinho... Filme favorito é Creed, programas de TV favoritos são The 100 e Prison Break... Favorito videogame é FIFA 19… Adora ouvir Drake… Grande fã de batatas fritas."
+  },
+  {
+    "id": 6,
+    "name": "Aaron Long",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/a-long-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=7dcbc92276044ad880ecf0eb5b7b9e05&amp;hash=C79FF96832704AB100E8EB2CDA181D42",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/white-headshots-june-2022-camp/aaron-long-transparent/longaaron-white-670-transparent.ashx?mw=1440&rev=3c792e0c748e4ef9909ab874f457cced"
+    },
+    "position": "defender",
+    "numbers": 5,
+    "dateOfBirth": "Oct 12 1992",
+    "hometown": "Oak Hills, CA",
+    "height": "6'1",
+    "club": "New York Red Bulls",
+    "appearance": 27,
+    "cleansheets": 3,
+    "wins": 2,
+    " description": " "
+  },
+  {
+    "id": 7,
+    "name": "Mark McKenzie",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/m-mckenzie-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=e8a64dcb7b4648a5b5528113151107c8&amp;hash=45D5884F4A13CF2C75B65C13B494606E",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/deandre-yedlin/deandreyedlinp1tier1v4desktop2x/mckenzie-playerprofiletier1playernamejpg55.ashx?mw=1440&rev=e408d654990e4d72ae746b56ebffac54"
+    },
+    "position": "Defender",
+    "numbers": 16,
+    "dateOfBirth": "Feb 25 1999",
+    "hometown": "Berar, DE",
+    "height": "6'0",
+    "club": "Krc Genk (Belgium)",
+    "appearance": 9,
+    "goals": 0,
+    "wins": 1,
+    " description": " "
+  },
+  {
+    "id": 8,
+    "name": "Erik Palmer-Brown",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/epb-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=f2f00778196b42668778ac7a46257882&amp;hash=0F1717D01DC20F7643671B2A275B7D9B",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/epb-player-overview.ashx?mw=1440&rev=72d8a9ec777746648b24c3df2e277584"
+    },
+    "position": "Defender",
+    "numbers": 12,
+    "dateOfBirth": "Apr 24 1997",
+    "hometown": "Lee's Smmit, MO",
+    "height": "61",
+    "club": "troyes (France)",
+    "appearance": 4,
+    "cleansheets": 0,
+    "wins": 0,
+    " description": " "
+  },
+  {
+    "id": 9,
+    "name": "Joseph Scally",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/joe-scally-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=c27d9677943448dbba59b1c51319651e&amp;hash=EE7B9EA86A3FF51EE88DE2ED605E67BB",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/white-headshots-june-2022-camp/scally_joe-white-player-image_transparent.ashx?mw=1440&rev=66a5372bcb3840009a1215711a937dd3"
+    },
+    "position": "Defender",
+    "numbers": 26,
+    "dateOfBirth": "Dec 31 2002",
+    "hometown": "Lake Grove, NY",
+    "height": "6'0",
+    "club": "Borussia Mönchengladbch",
+    "appearance": 2,
+    "goals": 0,
+    "wins": 0,
+    " description": " "
+  },
+  {
+    "id": 10,
+    "name": "Sam Vines",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/sam-vines-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=581d9defcef348df8dd6891fb8c25ac7&amp;hash=2DB636D4FF93964E55A402774BA2B224",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/teams/player-cards/u-17/bello-playerprofiletier2playernamejpg55/sam-vines-red-playerprofiletier1playernamejpg55.ashx?mw=1440&rev=e091dc7c16654712a7e6d0e17ff1e61f"
+    },
+    "position": "Defender",
+    "numbers": 6,
+    "dateOfBirth": "May 31 1999",
+    "hometown": "Colorado Springs, CO",
+    "height": "5'10",
+    "club": "Royal Antwerp (Belgium)",
+    "appearance": 9,
+    "goals": 1,
+    "wins": 1,
+    " description": " "
+  },
+  {
+    "id": 11,
+    "name": "DeAndre Yedlin",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/deandre-yedlin-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=a3ef5a9a9c384a54939013205b70285c&amp;hash=F2BE70901578D85E9CB9D04FA766F5BE",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/mnt/deandre-yedlin/deandreyedlinp1tier1v4desktop2x/yedlin-playerprofiletier1playernamejpg55.ashx?mw=1440&rev=1e713a88b44a46fca87c5a07aa0f628d"
+    },
+    "position": "Defender",
+    "numbers": 22,
+    "dateOfBirth": "Jul 09 1993",
+    "hometown": "Seattle, WA",
+    "height": "5'8",
+    "club": "Inter Miami CF",
+    "appearance": 73,
+    "goals": 0,
+    "wins": 8,
+    " description": "Ritmo incrível e jogo altruísta definiram a carreira profissional do lateral-direito DeAndre Yedlin, bem como seu tempo com o MNT. O potencial de Yedlin ficou imediatamente claro durante seu tempo no Emerald City FC e no clube Crossfire Premier da US Soccer Development Academy na área de Seattle, onde ele chamou a atenção com seu estilo de jogo exuberante e energia que aparentemente poderia durar dias. Com a capacidade de jogar na linha de trás e como ala, Yedlin fez parte da lista dos EUA na Copa do Mundo Sub-20 da FIFA 2013 e foi para o MNT em fevereiro seguinte, levando-o a fazer o Mundial da FIFA 2014 Elenco da Copa. Tendo ultrapassado a marca de meio século de internacionalizações em 2018, Yedlin tem um forte currículo de experiência ao entrar nos primeiros anos de sua carreira. Desde então, ele apareceu nas aparições do USMNT na Copa Ouro da Concacaf de 2015, na Copa América Centenário de 2016 e nas Eliminatórias da Copa do Mundo da FIFA de 2018. Ele alcançou sua 50ª internacionalização apenas quatro anos em sua carreira internacional em uma vitória por 1-0 contra o Paraguai em 27 de março de 2018 em Cary, NC. Por todas as contas, não demorou muito para a carreira profissional de Yedlin decolar. O ano de 2011 o viu assinar uma carta de intenção para jogar pelo Akron, onde foi um jogador-chave na linha de trás, ganhando honras do All-MAC First Team nas duas temporadas em que jogou com os Zips. Em 201, ele se tornou o primeiro jogador caseiro na história do Seattle Sounders FC, mas após sua atuação na Copa do Mundo de 2014, sua permanência no CenturyLink Field foi curta. As rotinas pré-jogo também são uma parte fundamental para se sentir bem. Yedlin toma um café da manhã com feijão, torradas e geleia no café local Dean and Daniela's antes de cada jogo em casa. As rotinas estão claramente funcionando, pois ele se tornou um regular com Newcastle o tempo todo se estabelecendo como um favorito dos fãs."
+  },
+  {
+    "id": 12,
+    "name": "Walker Zimmerman",
+    "UrlImg": {
+      "capa": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/players/player-thumbnails/mnt/22-23-jersey/walker-zimmerman-sitecore-image-580x580.ashx?h=580&amp;la=en-US&amp;w=580&amp;rev=dffec967db0f4b31bcfdd8beead7aaa8&amp;hash=F92B3C24422C91B85EEA59C5D8E3D213",
+      "perfil": "https://cdn.ussoccer.com/-/jssmedia/project/ussf/teams/player-cards/u-17/bello-playerprofiletier2playernamejpg55/walker-zimmerman-red-playerprofiletier1playernamejpg55.ashx?mw=1440&rev=e17c17dfef9e4237ab716d1a7f3474b6"
+    },
+    "position": "Defender",
+    "numbers": 3,
+    "dateOfBirth": "May 19 1993",
+    "hometown": "lawshville SC",
+    "height": "6'2",
+    "club": " Nashville SC",
+    "appearance": 32,
+    "cleansheets": 3,
+    "wins": 1,
+    " description": " "
+  }
+]
